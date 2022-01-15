ff7480e9aa522ba14e50d00c1f06783697f484fa
Andres Pena <penchjpc@gmail.com>
2022-01-15 12:15:56 -0500

    html elements to Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import react from 'react';
+
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Gallery from './components/Search';
+import Gallery from './components/pages/Gallery';
 
 export default function App() {
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import Search from './Search'
 
-function Header() {
+export default function Header() {
     return (
         <div>
-            
+             {/* <!-- Header --> */}
+            <div className="row header">
+                {/* <!-- name --> */}
+              <div className="col-12 col-sm-6 col-md-4 col-lg-4">
+                <h1 id="AppName">SpaceAweGram</h1>
+              </div>              
+                <Search/>             
+            </div>
+            {/* <!-- Ribbon --> */}
+            <div className="row ribbon-wrapper">
+              {/* <!-- subtitle --> */}
+                <div className="col ribbon">                  
+                  <h3 id="subtitle">Hi ! I am a web developer passionate about mountains, creating and drawing.</h3>
+                </div>              
+              </div>
         </div>
     )
 }
 
-export default Header
+
