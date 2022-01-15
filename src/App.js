aeab9f1448689f93f76db503a5d9af1eb066afc7
Andres Pena <penchjpc@gmail.com>
2022-01-15 12:35:47 -0500

    installed bootstrap and added CSS stylesheet to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Gallery from './components/pages/Gallery';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function App() {
 
   return (
