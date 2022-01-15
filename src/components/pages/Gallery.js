d5a1d242aa22bebee2714b63c70a2e5ccbf01e78
Andres Pena <penchjpc@gmail.com>
2022-01-15 12:39:52 -0500

    imported Card component in Gallery.js

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Card from '../Card'
 
 function Gallery() {
+    const cards = []
+
     return (
         <div>
-            
+            {cards.map((card) =>{
+               return <Card title={card.title} image={card.image} date={card.date} description={card.description} />
+             })}
         </div>
     )
 }
