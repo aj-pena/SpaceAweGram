b60e3a3b6045227819cf822548969c8b1c40ee46
Andres Pena <penchjpc@gmail.com>
2022-01-17 22:52:51 -0500

    consolidated Gallery and Search into one component Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-import React, {useState} from 'react'
-import Gallery from './pages/Gallery'
+import React, {useState, useEffect} from 'react'
+import Card from './Card'
 
 function Search() {
-    const [query, setQuery] = useState('earth')
+    const baseUrl = 'https://images-api.nasa.gov';
     let str = '';
+    const [query, setQuery] = useState('earth')
+    const [Darray, setDArray] = useState([])
+
+    useEffect(()=>{
+        //  Fetching data from API and storing in Array "Items" if the media type is "image"
+         fetch(`${baseUrl}/search?q=${query}`)
+          .then((response) => {
+            console.log(response)
+            return response.json()
+          })
+          .then((json) => {
+              json.collection.items.map((item)=>{
+                if (item.data[0].media_type === 'image'){
+                  // item.setAttribute('key', item.data[0].nasa_id)
+                  setDArray(Darray => [...Darray, item])               
+                }         
+              })        
+          })
+      }, [query, Darray]);
+
     function handleChange(e){
         str = e.target.value;
         console.log(str)
@@ -13,6 +33,11 @@ function Search() {
         setQuery(str)
     }
     
+    
+            
+    
+      
+
 
 
     return (
@@ -20,10 +45,15 @@ function Search() {
             
             <input onChange={handleChange} className="form-control mr-sm-2" type="text" placeholder="Type a keyword" aria-label="Search" name='search'/>
             <button onClick={handleClick} className="btn btn-outline-success my-2 my-sm-0 search-btn">Search</button>
+            <div className='cards-container'>
+                {Darray.map((i) =>{
+                    return <Card key={i.data[0].nasa_id} title={i.data[0].title} image={i.links[0].href} date={i.data[0].date_created} description={i.data[0].description} id={i.data[0].nasa_id} />
+                })}
+        </div>
             
-            <div>
+            {/* <div>
                 <Gallery data={query}/>
-            </div>          
+            </div>           */}
 
         </div>
     )
