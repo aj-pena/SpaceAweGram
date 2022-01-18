32c42897d5909ff0abbe9cc3b782abfd44500751
Andres Pena <penchjpc@gmail.com>
2022-01-17 23:23:34 -0500

    added heart button that changes onClick

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Card({ image, title, date, description, id }) {
+    const [heart, setHeart] = useState('♡');
+    function handleLike(){
+        if(heart === '♡'){
+            setHeart('♥')
+        }else {
+            setHeart('♡')
+        }
+    }
+
     return (
         <div className="card" key={id}>
             <img src={image} className="card-img-top" alt="preview"/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
+                <br></br>
                 <p className='project-description card-text'>{description}</p>
-                <p>{date}</p>                                                               
+                <h6>Date: {date}</h6>
+                <button className='heart-btn' onClick={handleLike}>{heart}</button>                                                               
             </div>                  
         </div>
     )
